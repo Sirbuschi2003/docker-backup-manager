883fe16ef2b0c50ef9aa7ba1ea9d4e88df768d5b
Thomas Busche <thomas@buschehome.de>
2026-09-05 23:52:14 +0200

    Bump version to 1.4.2

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ import secrets
 from pathlib import Path
 from typing import Optional
 
-APP_VERSION = "1.4.1"
+APP_VERSION = "1.4.2"
 GITHUB_REPO = "Sirbuschi2003/docker-backup-manager"
 
 import pytz
